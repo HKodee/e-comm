ffc4377f3cca0e614c59578dcaeffd0ca0593013
Harsh Kandpal <harshksndpal1384@gmail.com>
2026-06-01 20:57:14 +0530

    added essentials

--- app.py
+++ app.py
@@ -7,10 +7,30 @@ lm = LoginManager()
 
 def create_app():
     app = Flask(__name__)
+
+    app.config['SECRET_KEY'] =  'legit-key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///memory.db:"
+    app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+
+    db.init_app(app)
+    lm.init_app(app)
+    lm.login_view = "login"
     
     @app.route('/')
     def index():
         return render_template("index.html")
+    
+    @app.route('/')
+    def register():
+        return render_template("register.html")
+    
+    @app.route('/')
+    def login():
+        return render_template("login.html")
+    
+    @lm.user_loader
+    def login_user(user_id):
+        return
     
     return app
 
