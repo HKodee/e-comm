ee14e77ef562c3a4daa09e886de3879dcab515a5
Harsh Kandpal <harshksndpal1384@gmail.com>
2026-06-04 17:43:06 +0530

    now errors will be displayed on page

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
 from flask_login import LoginManager,UserMixin
+import re
 
 db = SQLAlchemy()
 lm = LoginManager()
