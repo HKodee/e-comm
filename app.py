a0981d563970f52676130a7f5b937289c1929874
Harsh Kandpal <harshksndpal1384@gmail.com>
2026-07-22 17:37:57 +0530

    admin dashboard and routes created and joined

--- app.py
+++ app.py
@@ -29,6 +29,8 @@ def create_app():
     lm.login_view = "auth.login"
     
     from app.routes.auth import auth_bp
+    from app.routes.admin import admin_bp
+    app.register_blueprint(admin_bp)
     app.register_blueprint(auth_bp)
 
     @app.route("/health/db")
--- app/routes/admin.py
+++ app/routes/admin.py
@@ -1,2 +1,15 @@
-from .auth import auth_bp
-from .admin import admin_bp
+
+from flask import Blueprint, render_template
+from flask_login import login_required
+
+admin_bp = Blueprint(
+    "admin",
+    __name__,
+    url_prefix="/admin"
+)
+
+
+@admin_bp.route("/dashboard")
+@login_required
+def dashboard():
+    return render_template("admin/dashboard.html")
