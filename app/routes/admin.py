c7e2b56e7a42e5855fc4f92c674f0293c356d399
Harsh Kandpal <harshksndpal1384@gmail.com>
2026-07-24 08:21:34 +0530

    User table structure created

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -1,6 +1,7 @@
 from app.decorators import admin_required
 from flask import Blueprint, render_template
 from flask_login import login_required
+from app.models.User import User
 
 admin_bp = Blueprint(
     "admin",
@@ -14,3 +15,15 @@ admin_bp = Blueprint(
 @admin_required
 def dashboard():
     return render_template("admin/dashboard.html")
+
+@admin_bp.route("/users")
+@login_required
+@admin_required
+def users():
+
+    all_users = User.query.order_by(User.id).all()
+
+    return render_template(
+        "admin/users.html",
+        users=all_users
+    )
